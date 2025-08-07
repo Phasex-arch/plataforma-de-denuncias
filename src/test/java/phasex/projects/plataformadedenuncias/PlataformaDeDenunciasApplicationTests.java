d79a9b6346052156057fce4b8807aecfef0fb3aa
RicardoFernandes2004 <ricardo.fernandes02082004@gmail.com>
2025-08-07 09:09:40 -0300

    first commit

---
+++ src/test/java/phasex/projects/plataformadedenuncias/PlataformaDeDenunciasApplicationTests.java
@@ -0,0 +1,13 @@
+package phasex.projects.plataformadedenuncias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlataformaDeDenunciasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
