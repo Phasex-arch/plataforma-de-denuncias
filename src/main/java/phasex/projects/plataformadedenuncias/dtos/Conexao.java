9cdc7e8e42b643883d84a8081fdd66bc0040eb13
Khadija Lima <khadijalima2005@gmail.com>
2025-08-08 21:58:38 -0300

    "feat: configurar conexão com PostgreSQL usando dotenv e variáveis de ambiente"

---
+++ src/main/java/phasex/projects/plataformadedenuncias/dtos/Conexao.java
@@ -0,0 +1,28 @@
+package phasex.projects.plataformadedenuncias.dtos;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Conexao {
+    public static void main(String[] args) {
+        try {
+            Dotenv dotenv = Dotenv.load();
+
+            String usuario = dotenv.get("DB_USER");
+            String senha = dotenv.get("DB_PASSWORD");
+            Connection conexao = DriverManager.getConnection(
+                    "jdbc:postgresql://localhost:5432/plataforma_denuncia",
+                    usuario,
+                    senha
+            );
+            if (conexao != null) {
+                System.out.println("Conectado com sucesso!");
+            } else {
+                System.out.println("Conexão falhou.");
+            }
+        } catch (Exception e) {
+            System.out.println("Erro: " + e.getMessage());
+        }
+    }
+}
